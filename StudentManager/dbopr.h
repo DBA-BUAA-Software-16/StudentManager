53527acf2249e9447181ee0c7e471a550c327b11
LW-Ricarido <liweicq@buaa.edu.cn>
2018-12-28 20:58:51 +0800

    finish signin signup

--- StudentManager/dbopr.h
+++ StudentManager/dbopr.h
@@ -2,16 +2,21 @@
 #define DBOPR_H
 
 #include <QObject>
-
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QVariant>
+#include <QDebug>
+#include <QSqlRecord>
+#include <QDateTime>
 class DBOpr : public QObject
 {
     Q_OBJECT
 public:
     explicit DBOpr(QObject *parent = nullptr);
-    static bool signIn(QString Usr, QString pw,QString name, int type);
-    static bool signUp(QString Usr, QString pw,QString name, int type);
-    static bool changePassword(QString usr, QString oldpw, QString npw, int type);
-    static bool createCourse(int courseHour, int createHour, QDateTime beginTime,QDateTime endTime, QDate term,int stuNum,int tid);
+    static bool signUp(int type,QString Usr, QString pw,QString name);
+    static int signIn(int type,QString Usr, QString pw);
+    static bool changePassword(int type, int id, QString npw);
+    static bool createCourse(int tid,int courseHour, int createHour, QDateTime beginTime,QDateTime endTime,int stuNum);
     static bool chooseCourse(int stuID,int courseID);
     static bool dropCourse(int stuID, int courseID);
     static bool setGrade(int stuID,int courseID,int score);
--- StudentManager/main.cpp
+++ StudentManager/main.cpp
@@ -1,11 +1,27 @@
 #include "mainwindow.h"
 #include <QApplication>
-
+#include <QSqlDatabase>
+#include <QMessageBox>
+#include <QDateTime>
+#include "dbopr.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QSqlDatabase db=QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("140.143.30.64");
+    db.setUserName("root");
+    db.setPassword("root");
+    db.setDatabaseName("dba");
 
-    return a.exec();
+    if(db.open())
+    {
+        DBOpr::createCourse(1,20,2,QDateTime::currentDateTime(),QDateTime::currentDateTime(),20);
+        MainWindow w;
+        w.show();
+        return a.exec();
+    }
+    else
+    {
+        QMessageBox::warning(NULL,"warning","数据库打开失败",QMessageBox::Yes);
+    }
 }
